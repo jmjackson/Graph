807d87bce0fdbe107f41261105972c765592ce44
jp50000 <44383527+jp50000@users.noreply.github.com>
2020-09-19 02:36:10 -0700

    Adjuntar Archivo

--- Grap.Principal/Views/Registro.cs
+++ Grap.Principal/Views/Registro.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,15 @@ namespace Grap.Principal.Views
         private void BtnRegistrar_Click(object sender, EventArgs e)
         {
             var cd=TxtCode.Text;
+            //var Path = "C:Resources\\images" + ".jpg";
+            var path = "~/Resources/images/";
+            if (Directory.Exists(path))
+            {
+                //Bitmap bmp;
+                String paths = Application.StartupPath;
+                //bmp.Save(openFileDialog1.FileName, "Resources\\images\\" + Path.GetFileName(openFileDialog1.FileName));
+                File.Copy(openFileDialog1.FileName, "Resources\\images\\" + Path.GetFileName(openFileDialog1.FileName));
+            }
             Clients c = new Clients()
             {
                 Code=cd,
@@ -48,17 +58,52 @@ namespace Grap.Principal.Views
 
         private void BunifuImageButton1_Click(object sender, EventArgs e)
         {
+            //try
+            //{
+            //    //Filtro de los archivos que se veran en este caso solo veremos pueros .pdf
+            //    openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            //    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //    {
+            //        //Aqui seleciona el Archivo y se ponen el el cuadro de texto
+            //        TxtImage.Text = openFileDialog1.FileName;
+            //        File.Copy(openFileDialog1.FileName, "Resources\\images\\" + Path.GetFileName(openFileDialog1.FileName));
+
+
+
+            //    }
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    MessageBox.Show("No se Cargo la Imagen " + ex.ToString());
+            //}
+
+            ////Propuesta 2
+            //try
+            //{
+            //    //////    //Filtro de los archivos que se veran en este caso solo veremos pueros .pdf
+            //    openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            //    openFileDialog1.ShowDialog();
+            //    DialogResult dr = openFileDialog1.ShowDialog();
+            //    if (DialogResult == DialogResult.OK)
+            //    {
+            //        TxtImage.Text = openFileDialog1.FileName;
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    MessageBox.Show("No se Cargo la Imagen " + ex.ToString());
+            //}
             try
             {
-                //Filtro de los archivos que se veran en este caso solo veremos pueros .pdf
+                //////    //Filtro de los archivos que se veran en este caso solo veremos pueros .pdf
                 openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    //Aqui seleciona el Archivo y se ponen el el cuadro de texto
                     TxtImage.Text = openFileDialog1.FileName;
-
                 }
-
             }
             catch (Exception ex)
             {
