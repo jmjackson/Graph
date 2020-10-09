7793561b1bcebaf8cd62863a80aa7cab7a49a318
jp50000 <44383527+jp50000@users.noreply.github.com>
2020-10-08 18:10:28 -0700

    Draw

---
+++ Views/DibujoM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mine.Views
+{
+    public struct DibujoM
+    {
+        public int x, y, w, h; // Posicion x, y | Largo y ancho
+        public string tag; // El texto
+        public Color line;
+    }
+}
